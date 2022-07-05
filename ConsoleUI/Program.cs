a2fa4389c09a2ee1b9a66e6467cd4e74f2f49044
Emopusta <emreduman225@hotmail.com>
2022-07-05 13:02:37 +0300

    ders10

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,15 +32,21 @@ namespace ConsoleUI
 
         private static void ProductTest(ProductManager productManager)
         {
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+                
             }
-            Console.WriteLine("--------------------------------------------");
-            foreach (var product in productManager.GetAllByCategoryId(8))
+            else
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(result.Message);
             }
+            
         }
     }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+        public string Message { get; }
+        public Result(bool success, string message):this(success)//bu constructor çalışınca this(success) sayesinde aşağıdaki de çalışır DRY!
+        {
+            Message = message;
+            
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+       
+    }
+}
